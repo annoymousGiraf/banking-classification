1629bc8392c994400c301d9b0305c0eb4940d981
guy.peleg <guy.peleg@myob.com>
2019-10-23 22:22:44 +1100

    delete TODO

--- src/main/kotlin/extention/StringExtentions.kt
+++ src/main/kotlin/extention/StringExtentions.kt
@@ -3,7 +3,7 @@ package extention
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-private val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss") //TODO: move to property
+private val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss") 
 
 fun String.toDate() : LocalDateTime{
     return LocalDateTime.parse(this, formatter)
